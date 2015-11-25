b3923d3c25f0fcd0348d1e5b48f2c301328cf4e4
Rifati <rhuseni@roc-dev.com>
2015-11-25 13:41:08 +0100

    ><

--- Garp.java
+++ Garp.java
@@ -13,94 +13,55 @@ public class Garp extends Actor
         private GreenfootImage imageRight;
         public boolean isDead = false;
         GreenfootImage noImg = new GreenfootImage(1, 1);
-        
-        
+       
         
         public Garp()
-        
         {
             imageLeft = new GreenfootImage("GarpLeft.png");
-            
-            
             imageRight = new GreenfootImage("GarpRight.png");
-            
-            setImage(imageRight); 
-            
-            
+            setImage(imageRight);    
         }
-        
         public void act() 
         {
             if (checkdeath() == false) {
                 Actor diamond;
-                
                 diamond = getOneObjectAtOffset(0, 0, diamonds.class);
                 checkExploded();
                 if(diamond != null) { //Als Garp een diamant ziet
                     getWorld().removeObject(diamond); //Verwijder de diamant van de wereld
-                    
-                }
-                
-                
+                }   
                 {if(Greenfoot.isKeyDown("right")) {
                     if(getImage() == imageLeft) {
-                        
-                        setImage(imageRight);
-                        
+                        setImage(imageRight);    
                     }
                     setRotation(0);
-                    move(5);
-                    
+                    move(5); 
                 }
                 if(Greenfoot.isKeyDown("left")) {
                     if(getImage() == imageRight) {
-                        
-                        setImage(imageLeft);
-                        
+                        setImage(imageLeft);   
                     }
                     setRotation(180);
-                    
-                    move(5);
-                    
+                    move(5);   
                 }
                 if(Greenfoot.isKeyDown("up")) {
-                    if(getImage() == imageLeft) {
-                        
-                        setImage(imageRight);
-                        
+                    if(getImage() == imageLeft) { 
+                        setImage(imageRight); 
                     }
                     setRotation(-90);
-                    
-                    move(5);
-                    
+                    move(5); 
                 }
-                
                 if(Greenfoot.isKeyDown("down")) {
                     if(getImage() == imageLeft) {
-                        
                         setImage(imageRight);
-                        
                     }
                     setRotation(90);
-                    
-                    move(5);    
-                    
-                    
-                    
-                    
-                }
-                
-                
-                
+                    move(5);      
+                } 
             }
-            
-            
             if (checkRock() == true) {move(-5);} 
-            
-            
         }
     }
-    
     public boolean checkRock() {
         Actor rock = getOneObjectAtOffset(0, 0, Rock.class);
         if(rock != null) {
@@ -108,7 +69,7 @@ public class Garp extends Actor
         }
         else {return false;}
     }
-public boolean checkdeath() {
+       public boolean checkdeath() {
         Actor gnome = getOneObjectAtOffset(0, 0, Gnome.class);
         if(gnome != null) {
             getWorld().removeObject(this);
--- MyWorld.java
+++ MyWorld.java
@@ -9,20 +9,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
 private GreenfootSound sound = new GreenfootSound("Zelda.mp3");
-
 public void started()
-
 {
-
  sound.playLoop();
-
 }
 public void stopped() 
-
  {
-
  sound.stop();
-
  }
     /**
      * Constructor for objects of class MyWorld.
@@ -30,30 +23,21 @@ public void stopped()
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world with 700x500 cells with a cell size of 1x1 pixels.
         super(700, 500, 1); 
         addObject(new Garp(), 50, 50);
-        addObject(new Gnome(), 350, 250);
-        
+        addObject(new Gnome(), 350, 250);       
         for(int teller = 0; teller < 10; teller ++) {
-
 addObject(new diamonds(), Greenfoot.getRandomNumber(700), 
-
 Greenfoot.getRandomNumber(500));
 }
         for(int teller = 0; teller < 10; teller ++) {
-
 addObject(new Rock(), Greenfoot.getRandomNumber(700), 
-
 Greenfoot.getRandomNumber(500));
 }
         for(int teller = 0; teller < 4; teller ++) {
-
 addObject(new Bomb(), Greenfoot.getRandomNumber(700), 
-
 Greenfoot.getRandomNumber(500));
-
 }
-
     }
 }
